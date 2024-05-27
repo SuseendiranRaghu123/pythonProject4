16132a0d5b48531cd4d044f09be73e3b01e17ffa
SuseendiranRaghu123 <suseendiran23@gmail.com>
2024-05-27 22:57:01 +0530

    vercel

--- myapp.py
+++ myapp.py
@@ -1,24 +1,24 @@
-from flask import Flask, render_template
-import sqlite3
-from waitress import serve
+    from flask import Flask, render_template
+    import sqlite3
+    from waitress import serve
 
-app = Flask(__name__)
+    app = Flask(__name__)
 
-@app.route('/')
-def index():
-    try:
-        conn = sqlite3.connect('data1.db')
-        conn.row_factory = sqlite3.Row
-        c = conn.cursor()
+    @app.route('/')
+    def index():
+        try:
+            conn = sqlite3.connect('data1.db')
+            conn.row_factory = sqlite3.Row
+            c = conn.cursor()
 
-        c.execute("SELECT * FROM data ORDER BY id DESC")
-        data = c.fetchall()
+            c.execute("SELECT * FROM data ORDER BY id DESC")
+            data = c.fetchall()
 
-        conn.close()
+            conn.close()
 
-        return render_template('index.html', data=data)
-    except sqlite3.Error as e:
-        return f"Error: {e}", 500
+            return render_template('index.html', data=data)
+        except sqlite3.Error as e:
+            return f"Error: {e}", 500
 
-if __name__ == '__main__':
-    app.run( host='0.0.0.0', port=5000)
+    if __name__ == '__main__':
+        app.run( host='0.0.0.0', port=5000)
